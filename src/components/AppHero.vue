0d1c315ae08396ee5218a67e55f2666a4cfc7353
Axhu99 <xhuliodreni@icloud.com>
2024-01-31 16:15:28 +0100

    add AppHero

---
+++ src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<template>
+    <div class="jumbotron">
+
+    </div>
+</template>
+
+<style scoped>
+.jumbotron {
+    background-color: dodgerblue;
+    width: 1080px;
+    height: 500px;
+    margin: 0 auto;
+}
+</style>
